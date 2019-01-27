c2f0eae33da8d3a4abd77c78471877e516fdcbce
dineshPallapa <dinesh@enroco.com>
2019-01-27 16:59:30 +0530

    Added logic for removing a number which is highest seconds among all calls

--- call_rate_calculator.rb
+++ call_rate_calculator.rb
@@ -1,12 +1,33 @@
-def log_parser(log_string)
+def solution(log_string)
   logs_hash = Hash.new{ |h,k| h[k] = [] }
   logs = log_string.split("\n")
   logs.each do |log|
     time, phone = log.split(",")
     logs_hash[phone] << time
   end
-  p logs_hash
-  return get_number_of_secods(logs_hash)
+  removed_number_log = update_string_to_secods_add_promo(logs_hash)
+  return get_number_of_secods(removed_number_log)
+end
+
+def update_string_to_secods_add_promo(call_log_hash)
+  convert_string_to_seconds = Hash.new{ |h,k| h[k] = [] }
+  call_log_hash.each do |phone, call_seconds|
+    call_seconds.each do |second|
+      convert_string_to_seconds[phone] << calculate_seconds_from_string(second)
+    end
+  end
+
+  convert_sum_of_seconds = Hash.new
+  convert_string_to_seconds.each do |phone, call_seconds|
+    if(call_seconds.length < 1)
+      convert_sum_of_seconds[phone] = call_seconds
+    else
+      convert_sum_of_seconds[phone] = sum_all_cost(call_seconds)
+    end
+  end
+  max_key = convert_sum_of_seconds.max_by { |phone, seconds| seconds }.first
+  call_log_hash.delete(max_key)
+  return call_log_hash
 end
 
 def get_number_of_secods(log_hash)
@@ -20,7 +41,6 @@ def get_number_of_secods(log_hash)
       end
     end
   end
-  p total_seconds
   return calculate_cost_for_a_call(total_seconds)
 end
 
@@ -44,7 +64,6 @@ def calculate_cost_for_a_call(total_seconds)
       end
     end
   end
-  p cost_for_seconds
   return sum_all_cost(cost_for_seconds)
 end
 
@@ -62,12 +81,12 @@ def seconds_to_time(seconds)
   [seconds / 3600, seconds / 60 % 60, seconds % 60].map { |t| t.to_s.rjust(2,'0') }.join(':')
 end
 
-def apply_promotion_for_a_call
-  #fucking logic I didn't understood.
-  #All calls to the phone number that the longest total duration of calls are free.
-  #In this case of tie,
-  #If more than one phone number share the longest total duration
-  #the promotion is applied only to the phone number whose numerical value is the smallest amongthe phone numbers
-end
+# def apply_promotion_for_a_call
+#   #fucking logic I didn't understood.
+#   #All calls to the phone number that the longest total duration of calls are free.
+#   #In this case of tie,
+#   #If more than one phone number share the longest total duration
+#   #the promotion is applied only to the phone number whose numerical value is the smallest amongthe phone numbers
+# end
 
-log_parser("00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090")
+solution("00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090")
